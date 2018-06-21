8762c2ef826f32f1bc1f0c3adb7d425866870dc8
Rich Abdill <richard.abdill@gmail.com>
2018-06-20 20:42:06 -0500

    Pagination works, authors scraped

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -1,31 +1,59 @@
 from requests_html import HTMLSession
 
+class author(object):
+	def __init__(self, given, surname):
+		self.given = given
+		self.surname = surname
+
+	def name(self):
+		return "{} {}".format(self.given, self.surname)
+
 class article(object):
 	# This function expects an "Element" object from requests_html
 	# that contains information about only one article.
 	def __init__(self, html):
-		self.title = self.get_title(html)
-		self.url = self.get_url(html)
+		self.html = html
+		self.find_title()
+		self.find_url()
+		self.find_authors()
 
-	@staticmethod
-	def get_title(html):
-		x = entry.find(".highwire-cite-title")
+	def find_title(self):
+		x = self.html.find(".highwire-cite-title")
 		# this looks weird because the title is wrapped
 		# in 2 <span> tags with identical classes:
-		return x[0].text
+		self.title = x[0].text
 
-	@staticmethod
-	def get_url(html):
-		return html.absolute_links.pop() # absolute_links is a set
+	def find_url(self):
+		self.url = self.html.absolute_links.pop() # absolute_links is a set
 
+	def find_authors(self):
+		entries = self.html.find(".highwire-citation-author")
+		self.authors = []
+		for entry in entries:
+			first = entry.find(".nlm-given-names")[0].text
+			last = entry.find(".nlm-surname")[0].text
+			self.authors.append(author(first, last))
 
-if __name__ == "__main__":
-	session = HTMLSession()
-	r = session.get("https://www.biorxiv.org/collection/bioinformatics")
-	entries = r.html.find(".highwire-article-citation")
+def determine_page_count(html):
+	# takes a biorxiv results page and
+	# finds the highest page number listed
+	return int(html.find(".pager-last")[0].text)
+
+def process_page(html):
+	entries = html.find(".highwire-article-citation")
 	articles = []
 	for entry in entries:
 		articles.append(article(entry))
+	return articles
 
-	for z in articles:
-		print(z.url)
+if __name__ == "__main__":
+	session = HTMLSession()
+	# we need to grab the first page to figure out how many pages there are
+	r = session.get("https://www.biorxiv.org/collection/bioinformatics")
+	results = process_page(r.html)
+	for p in range(1, determine_page_count(r.html)):
+		r = session.get("https://www.biorxiv.org/collection/bioinformatics?page={}".format(p))
+		results += process_page(r.html)
+
+	for x in results:
+		print(x.title)
