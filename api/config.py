31543c55da782f76aaaf96ecf0ae14d2a964d189
Rich Abdill <richard.abdill@gmail.com>
2018-09-07 22:50:34 -0500

    womp womp

--- api/config.py
+++ api/config.py
@@ -1,12 +1,8 @@
-# db = {
-#   "host": "rxdb",
-#   "db": "postgres",
-#   "user": "postgres",
-#   "password": "mysecretpassword"
-# }
 db = {
-  "host": "rxivistdev.cdbcwedfe1zx.us-east-1.rds.amazonaws.com",
-  "db": "rxdb",
-  "user": "root",
-  "password": "F26FE228z5IW7J3A1"
+  "host": "rxdb",
+  "db": "postgres",
+  "user": "postgres",
+  "password": "mysecretpassword"
 }
+
+page_size = 20
